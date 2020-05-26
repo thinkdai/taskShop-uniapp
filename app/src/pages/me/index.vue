40cd4a434e455dbe7d3cdb0d6fd18972d490e7d2
Alex-Li2018 <392064881@qq.com>
2020-05-26 23:15:09 +0800

    建立用户表

---
+++ app/src/pages/me/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <view>
+        我的
+        <button
+          size="defult"
+          type="defult"
+          form-type="submit"
+          disabled="false"
+          >
+          获取登录信息的接口
+        </button>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        uni.login({
+            success (res) {
+                if (res.code) {
+                    //发起网络请求
+                    // wx.request({
+                    //     url: 'https://test.com/onLogin',
+                    //     data: {
+                    //     code: res.code
+                    //     }
+                    // })
+                    conosle.log(res.code)
+                } else {
+                    console.log('登录失败！' + res.errMsg)
+                }
+            }
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
