cfe18939b0a86909625b7b8d9985e92d0a96ae69
Alex-Li2018 <392064881@qq.com>
2020-05-28 23:06:46 +0800

    调试微信登录信息的接口

--- service/routes/users.js
+++ service/routes/users.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const WXBizDataCrypt = require('../utils/WXBizDataCrypt');
+const { login, createUser, queryUser, deleteUser } = require('../controller/users')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 /* GET users listing. */
 router.get('/info', function(req, res, next) {
+    const { code: JSCODE, encryptedData , iv } = req.query;
     const secretId = '04d8b405a231b98377169894d85802d2';
     const APPID = 'wx2cc73b1092adab71';
-    const JSCODE = '023v1e0I0G4Rjc2ic72I0o4a0I0v1e05';
     const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${APPID}&secret=${secretId}&js_code=${JSCODE}&grant_type=authorization_code`
+    // 直调微信的服务器
     request({
         url: url,//请求路径
         method: "get",//请求方式，默认为get
-    }, function(error, response, body) {
-        console.log(response)
+    }, async function(error, response, body) {
+        // 非法code校验
+        const { errcode, errmsg, session_key: sessionKey, openid } = JSON.parse(body);
+        console.log(errcode, errmsg, sessionKey, openid);
+        // 请求异常
+        if (!sessionKey) {
+            res.json(
+                new ErrorModel(errmsg)
+            );
+        } else {
+            // 请求成功
+            console.log(APPID, sessionKey);
+            const pc = new WXBizDataCrypt(APPID + '', sessionKey + '');
+            const data = pc.decryptData(encryptedData , iv);
+            
+            res.json(
+                new SuccessModel(data, '登录成功')
+            );
+        }
     }); 
-    res.send('respond with a resource');
 });
 
 module.exports = router;
