ee3cb3de1649b6d5de732a922df5cf507136a2b9
Alex-Li2018 <392064881@qq.com>
2020-06-10 23:00:48 +0800

    feat：实现token校验

--- service/app.js
+++ service/app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 //引入token的操作,token的白名单
-// const tokenUtil = require("./utils/token");
-// const config = require('./conf/tokenApi');
-// const until = require('./utils/untils');
+const tokenUtil = require("./utils/token");
+const config = require('./conf/tokenApi');
+const until = require('./utils/untils');
 
 var pcRouter = require('./routes/pc/index');
 var appRouter = require('./routes/app/index');
@@ -17,39 +17,41 @@ var app = express();
 
 // 跨域配置 本地调试使用
 app.use(function(req, res, next) {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  // res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, wec-access-token, Set-Cookie");
-  res.header("Access-Control-Allow-Credentials", "true");
+	//设置允许跨域的域名，*代表允许任意域名跨域
+	// res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, wec-access-token, Set-Cookie");
+	res.header("Access-Control-Allow-Credentials", "true");
 
-  next();
+	next();
 });
 
 //app.use是Express拦截器的方法
 app.use(function(req, res, next) {
-  
-  // 拿取token 数据 按照自己传递方式写
-  let cookie = req.headers['http-jwt'];
-  next();
-  // let token = until.cookieToJson(cookie);
-  // // 检查token是否有效（过期和非法）
-  // let user = tokenUtil.checkToken(token);
-  // if (user) {
-  //     //将当前用户的信息挂在req对象上，方便后面的路由方法使用
-  //     req.user = user;
-      
-  //     // 续期
-  //     tokenUtil.setToken({user,res});
-
-  //     next(); //继续下一步路由
-  // } else {
-  //     //不需要登录态域名白名单
-  //     if (config.tokenApi.join(',').indexOf(req.path) == -1) {
-  //         next();
-  //         return;
-  //     }
-  //     res.json({ code: 401, message: '无效的token.' });
-  // }
+  // pc的token与app的token分开
+  if (req.originalUrl.indexOf('apiPc')) {
+    // 拿取token 数据 按照自己传递方式写
+    const cookie = req.headers['cookie'];
+    const token = until.cookieToJson(cookie);
+    // 检查token是否有效（过期和非法）
+    const user = tokenUtil.checkToken(token);
+    console.log(user);
+    if (user) {
+      //将当前用户的信息挂在req对象上，方便后面的路由方法使用
+      req.user = user;
+      // 续期
+      tokenUtil.setToken({user,res});
+      next(); //继续下一步路由
+    } else {
+      //不需要登录态域名白名单
+      if (config.tokenApi.indexOf(req.path) == -1) {
+        next();
+        return;
+      }
+      res.json({ code: 401, message: '无效的token.' });
+    }
+  } else {
+    console.log(111);
+  }
 });
 
 // 中间件
@@ -68,18 +70,18 @@ app.use('/apiApp', appRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
