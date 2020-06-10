b7c68e80565bc1154e16e0cddde1896204615af5
alex-li2019 <alexlismile@163.com>
2020-06-10 11:16:27 +0800

    chore：service-增加eslint检查node代码

---
+++ service/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+      "ecmaVersion": 2017,
+      "sourceType": "module"
+    },
+    env: {
+      node: true,
+      browser: false,
+      es6: true
+    },
+    extends: [
+      'eslint:recommended'
+    ],
+    settings: {
+    },
+    'globals': {
+        'module':true,
+        'process': true,
+        'require': true,
+        "__NODE_ENV__": true
+    },
+    // add your custom rules here
+    'rules': {
+        "semi": ["error", "always"],
+        // /*尾部逗号设置*/
+        'comma-dangle': ["error", "never"],
+        // /*缩进*/
+        /*禁止使用debugger*/
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        // 'no-unused-vars': ['error', { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+        /*禁止使用console（生产环境）*/
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        // /*要求 switch 语句中有 default 分支*/
+        'default-case': "error",
+        // /*禁用 eval()*/
+        'no-eval': "error"
+    }
+  };
+  
