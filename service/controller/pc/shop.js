7751a2b030261a1fd0a1f35f8ece1b825dd94dec
Alex-Li2018 <392064881@qq.com>
2020-06-10 22:27:33 +0800

    feat：service-创建商铺API实现

---
+++ service/controller/pc/shop.js
@@ -0,0 +1,71 @@
+const { exec } = require('../../db/mysql');
+
+//创建商铺
+const createShop = (shopInfo) => {
+    const { shopUrl, shopName, shopOwner } = shopInfo;
+    const createtime = new Date().toLocaleString();
+    let updatetime = createtime;
+
+    const sql = "insert into `shop` (shopUrl,shopName,shopOwner,createtime, updatetime)" +
+                `values ('${shopUrl}', '${shopName}', '${shopOwner}', '${createtime}', '${updatetime}')`;
+
+    return exec(sql).then(rows => { 
+            return rows || {};
+        })
+        .catch(res => {
+            console.log(res);
+            return res;
+        });
+};
+
+// 编辑商铺
+const editShop = (shopInfo) => {
+    const { id, shopUrl, shopName, shopOwner } = shopInfo;
+    let updatetime = new Date().toLocaleString();
+
+    const sql = "update `shop` set shopUrl = " + `'${shopUrl}',` +
+                "shopName = " + `'${shopName}',` +
+                "shopOwner = " + `'${shopOwner}',` +
+                "updatetime = " + `'${updatetime}'` +
+                `where id = ${id}`;
+
+    return exec(sql).then(rows => { 
+            return rows || {};
+        })
+        .catch(res => {
+            console.log(res);
+            return res;
+        });
+};
+
+// 查询店铺
+const queryShop = () => {
+    const sql = "select * from `shop`";
+    return exec(sql).then(rows => {
+        return rows || {};
+    })
+    .catch(res => {
+        return res;
+    });
+};  
+
+// 删除商铺
+const deleteShop = (id) => {
+
+    const sql = "DELETE FROM `shop` WHERE id = " + `${id}`;
+
+    return exec(sql).then(rows => { 
+            return rows || {};
+        })
+        .catch(res => {
+            console.log(res);
+            return res;
+        });
+};
+
+module.exports = {
+    createShop,
+    queryShop,
+    deleteShop,
+    editShop
+};
--- service/routes/pc/index.js
+++ service/routes/pc/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var userRouter = require('./users');
+var shopRouter = require('./shop');
 
 // 用户路由
 router.use('/user', userRouter);
+// 商铺路由
+router.use('/shop', shopRouter);
 
 module.exports = router;
