4b3cdbed513c4e053f1d81a00f17babab67fd119
Alex-Li2018 <392064881@qq.com>
2020-06-28 23:05:02 +0800

    feat：app-实现首页banner的效果

--- app/src/pages/index/index.vue
+++ app/src/pages/index/index.vue
@@ -1,7 +1,25 @@
 <template>
 	<view class="content">
-		<view>
-			<text class="title">{{title}}</text>
+		<!-- banner区域 -->
+		<view class="banner__warpper">
+			<swiper 
+				class="swiper" 
+				:autoplay="true" 
+				:interval="3000" 
+				:duration="1000"
+				:circular="true" 
+				:current="swiperCurrent" 
+				@change="changeSwiper"
+				@transition="transitionSwiper">
+				<swiper-item v-for="item in swiperImg" :key="item.id">
+					<image class="swiper-item" :src="item.image_url" mode="scaleToFill" @tap="jumpUrl(item)"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 折扣试用 -->
+		<view class="try-task__warpper">
+			<view class="left"></view>
+			<view class="right"></view>
 		</view>
 	</view>
 </template>
@@ -10,7 +28,12 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				swiperImg: [{
+					id: 1,
+					image_url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593364559547&di=5af0b26d071a711ea2c72fe973caadc3&imgtype=0&src=http%3A%2F%2Fimglf3.nosdn.127.net%2Fimg%2FL0lHRjFqbkVQa2pIQXFwYWE2WUd6Q3A4dUN5aFJhZlBOaTd0b3VDVFlQNUNTeGpQUGpZbGF3PT0.jpg%3FimageView%26thumbnail%3D1680x0%26quality%3D96%26stripmeta%3D0%26type%3Djpg%257Cwatermark%26type%3D2%26text%3Dwqkg',
+					url: ''
+				}]
 			}
 		},
 		onLoad() {
@@ -22,20 +45,24 @@
 	}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang="scss" scoped>
+.banner__warpper {
+	.swiper {
+		width: 690rpx;
+		height: 240rpx;
+		border-radius: 8rpx;
+		overflow: hidden;
+		margin-top: 20rpx;
+		position: relative;
+		margin: 0 auto;
+		.swiper-item {
+			width: 690rpx;
+			height: 240rpx;
+			border-radius: 8rpx;
+		}
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
+	.try-task__warpper {
+		padding: 20px 30px 0;
 	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+}
 </style>
