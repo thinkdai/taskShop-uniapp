c302908175845ad514c09f19c3ff3a0c736f58fe
Alex-Li2018 <392064881@qq.com>
2020-07-19 21:31:50 +0800

    chore：pc-修改构建端口为9000

--- pc/build/webpack.dev.js
+++ pc/build/webpack.dev.js
@@ -13,7 +13,7 @@ module.exports = smart(base,{
     },
     //必须安装webpack-dev-serve
     devServer: { //开发服务的配置
-        port: 8080, //端口
+        port: 9000, //端口
         progress: true, //进度条
         // contentBase: '../client', //找到对应的文件夹开启服务
         compress: true, //启动压缩
